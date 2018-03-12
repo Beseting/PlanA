326b5563a83cb7606b624926a5128da3d17cf086
YLMF-1706171523\Jabez <970540337@qq.com>
2018-03-12 18:38:42 +0800

    优化BaseActivity

--- plib/src/main/java/com/cdbhe/plib/base/BaseActivity.java
+++ plib/src/main/java/com/cdbhe/plib/base/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.cdbhe.plib.base;
 
+import android.app.Activity;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
@@ -22,6 +23,7 @@ import com.cdbhe.plib.utils.ActivityStack;
 import com.cdbhe.plib.utils.LoadingDialog;
 import com.cdbhe.plib.utils.LogUtils;
 import com.cdbhe.plib.utils.MeasureUnitTranUtil;
+import com.jaeger.library.StatusBarUtil;
 
 /**
  * Activity基类
@@ -126,6 +128,13 @@ public abstract class BaseActivity extends BasePermissionsActivity {
      */
     public interface OnTitleBarListener{
         void clickMore(View view);
+    }
+
+    /**
+     * 显示标题栏
+     */
+    protected void showTitleBar(){
+        rlTitleBar.setVisibility(View.VISIBLE);
     }
 
     /**
