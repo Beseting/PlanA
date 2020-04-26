c09d8f0a0c7adf5b43b5df24fb90143e2106a2b2
JNCV4DYB3JZ82QK\Kevin <970540337@qq.com>
2020-04-26 16:16:01 +0800

    去除demo中的bugly

--- app/src/main/java/com/cdbhe/plana/main/App.java
+++ app/src/main/java/com/cdbhe/plana/main/App.java
@@ -4,8 +4,6 @@ import android.app.Application;
 
 import com.cdbhe.plib.http.retrofit.HttpConfig;
 import com.cdbhe.plib.http.retrofit.RetrofitClient;
-import com.cdbhe.plib.utils.AppUtils;
-import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Created by Kevin on 2018/3/5.
@@ -16,13 +14,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        // 初始化Bugly
-        String processName = AppUtils.getProcessName(android.os.Process.myPid());// 获取当前进程名
-        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());// 设置是否为上报进程
-        strategy.setUploadProcess(processName == null || processName.equals(getApplicationContext().getPackageName()));
-        CrashReport.initCrashReport(getApplicationContext(), "420f93d2c2", true, strategy);
-
         //初始化Retrofit
         RetrofitClient.getInstance().init(new HttpConfig.Builder().setBaseUrl("https://restapi.amap.com/").build());
     }
